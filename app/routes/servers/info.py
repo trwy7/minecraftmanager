7698959704f56ad3363f281e41b6c1891b01d15c
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-02-09 16:19:47 -0700

    Functional server page

--- app/routes/servers/info.py
+++ app/routes/servers/info.py
@@ -1,4 +1,4 @@
-from app import app, Server, require_login
+from app import app, Server, require_login, get_server_status
 from flask import render_template
 
 @app.route('/server/<int:server_id>', methods=['GET'])
@@ -8,4 +8,4 @@ def get_server_info(server_id):
     if not server:
         return {'error': 'Server not found'}, 404
 
-    return render_template('server.html', server=server)
+    return render_template('server.html', server=server, server_status=get_server_status(server))
