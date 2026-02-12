71b62d1d9c63084f2acde331329215de68128cbd
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-02-12 13:52:19 -0700

    Change run command function on the server page Makes it a command bar, and makes it send to STDIN instead of RCON

--- app/routes/servers/action.py
+++ app/routes/servers/action.py
@@ -1,4 +1,4 @@
-from app import app, Server, socketio, start_server, stop_server, run_command, require_login
+from app import app, Server, socketio, start_server, stop_server, run_command, require_login, send_stdin
 from flask import request
 
 @socketio.on('start_server')
@@ -24,6 +24,14 @@ def handle_restart_server(data):
         stop_server(server)
         print(f"Server {server.name} stopped starting back up...")
         start_server(server)
+
+@socketio.on('send_command')
+def handle_send_command(data):
+    server_id = data.get('server_id')
+    command = data.get('command')
+    server = Server.query.get(server_id)
+    if server and command:
+        send_stdin(server, command + "\n")
 
 @app.route('/servers/<int:server_id>/command', methods=['POST'])
 @require_login
